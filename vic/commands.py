e880e4af11a3f0456cf5617bed3e687b4d436ca9
Tommaso Vicario <tommaso.vicario@gmail.com>
2026-03-17 14:39:47 +0100

    Implemented vic init

--- vic/commands.py
+++ vic/commands.py
@@ -1,7 +1,21 @@
-
+import os
 
 def cmd_init():
-    print("init")
+    path = os.getcwd()
+    try:
+        os.makedirs(".vic", exist_ok=False)
+        os.makedirs(".vic/objects",exist_ok=True)
+        os.makedirs(".vic/refs",exist_ok=True)
+        os.makedirs(".vic/refs/heads",exist_ok=True)
+        os.makedirs(".vic/refs/tags",exist_ok=True)
+        with open(".vic/HEAD", "a") as f:
+            f.write("ref: refs/heads/main")
+    except FileExistsError:
+        print("La cartella esisteva già!")
+
+    
+    print("Empty repository inizialized in " + path)
+    
 
 def cmd_add(files):
     print("add")
